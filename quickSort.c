2d9b3a586e601c77774a2c1a4170dbac56da0a1f
Rishabh <rishabhky23@gmail.com>
2024-02-25 15:52:38 +0530

    few more progs

--- quickSort.c
+++ quickSort.c
@@ -42,7 +42,14 @@ void printArray(int arr[], int size){
     printf("\n");
 }
 
-void main(){
+int main(){
+    int arr[]={10,7,8,9,1,5};
 
+    int n=sizeof(arr)/sizeof(arr[0]);
+    quickSort(arr,0,n-1);
 
+    printf("Sorted array : \n");
+    printArray(arr,n);
+    
+    return 0;
 }
